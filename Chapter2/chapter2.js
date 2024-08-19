cd32fa3674454f90493c49e6a132055b3ef24d4f
iMubashirSyed <121051599+iMubashirSyed@users.noreply.github.com>
2024-08-19 15:56:17 +0500

    Add files via upload

---
+++ Chapter2/chapter2.js
@@ -0,0 +1,36 @@
+// Task 1
+var username;
+
+// Task 2
+var myName = "Syed Mubashir Ali";
+
+// Task 3
+var message = "Hello, World!";
+alert(message);
+
+// Task 4
+var name1 = "Syed Mubashir Ali";
+var age = 19;
+var course = "Web and App dev";
+alert(name1);
+alert(age);
+alert(course);
+
+// Task 5
+var pizza = "Pizza\nPizz\nPiz\nPi\nP";
+alert(pizza);
+
+// Task 6
+var email = "example" + "@example.com";
+alert(email);
+
+// Task 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+// Task 8
+console.log("Yah! I can write HTML content through JavaScript");
+
+// Task 9
+var sign = "--------@!#!@--------";
+alert(sign);
