1dea856e05861228604994ecc4977b03c20be0e7
iMubashirSyed <121051599+iMubashirSyed@users.noreply.github.com>
2024-08-19 15:56:54 +0500

    Add files via upload

---
+++ Chapter3/chapter3.js
@@ -0,0 +1,18 @@
+// Task 1
+var age = 19;
+alert("I am " + age + " years old");
+
+// Task 2
+var times = 14;
+alert("You have visited this site for " + times + " times");
+
+// Task 3
+var birthyear = 2004;
+console.log("My birth year is " + birthyear);
+console.log("Data type of declared variable is number");
+
+// Task 4
+var visitorName = "George";
+var product = "T-Shirts";
+var quantity = 7;
+alert(visitorName+" ordered "+quantity+" "+product+" from Eden Apparels.");
