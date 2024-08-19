a9462fce904c424f338d57759ac00241b4ec8598
iMubashirSyed <121051599+iMubashirSyed@users.noreply.github.com>
2024-08-19 15:57:50 +0500

    Add files via upload

---
+++ Chapter5/chapter5.js
@@ -0,0 +1,107 @@
+// Task 1
+var num1 = 5;
+var num2 = 10;
+var sum = num1 + num2;
+console.log("Sum of " + num1 + " and " + num2 + "is " + sum);
+
+// Task 2
+// subtraction
+var sub1 = 5;
+var sub2 = 10;
+var sub = sub2 - sub1;
+console.log("Subtraction of " + sub2 + " from " + sub1 + "is " + sub);
+
+// multiplication
+var mul1 = 5;
+var mul2 = 10;
+var mul = mul1 * mul2;
+console.log("Multiplication of " + mul1 + " and " + mul2 + "is " + mul);
+
+// division
+var div1 = 5;
+var div2 = 10;
+var div = div2 / div1;
+console.log("Division of " + div2 + " from " + div1 + "is " + div);
+
+// Task3
+var name1;
+console.log("Value after variable declaration is "+name1);
+
+name1 = 5;
+console.log("Initial value: "+name1);
+name1++; 
+console.log('Value after increment'+name1);
+name1 = name1 + 7;
+console.log("Value afte addition is "+name1);
+name1--;
+console.log("Value after decreament is "+name1);
+console.log("The remainder is "+name1/3);
+
+// Task 4
+var ticket = 600;
+console.log("Total cost to buy 5 tickets to a movie is "+ticket*5+"PKR");
+
+// Task 5
+var table = 4;
+console.log("Table of 4:");
+console.log(table+"x1"+"="+table);
+console.log(table+"x2"+"="+table*2);
+console.log(table+"x3"+"="+table*3);
+console.log(table+"x4"+"="+table*4);
+console.log(table+"x5"+"="+table*5);
+console.log(table+"x6"+"="+table*6);
+console.log(table+"x7"+"="+table*7);
+console.log(table+"x8"+"="+table*8);
+console.log(table+"x9"+"="+table*9);
+console.log(table+"x10"+"="+table*10);
+
+// Task 6
+var C = 25;
+var F = 70;
+console.log(C+"C is "+((C*9/5)+32)+"F");
+console.log(F+"F is "+((F-32)*5/9)+"C");
+
+// Task 7
+var p1 = 650;
+var p2 = 100;
+var q1 = 3;
+var q2 = 7;
+var charges = 100;
+console.log("Price of item 1 is "+p1);
+console.log("Quantity of item 1 is "+q1);
+console.log("Price of item 2 is "+p2);
+console.log("Quantity of item 2 is "+q2);
+console.log("Total cost of your order is "+((p1*q1)+(p2*q2)));
+
+// Task 8
+var totalMarks = 980;
+var Obtained = 804;
+console.log("Percentage: "+((Obtained/totalMarks)*100));
+
+// Task 9
+var dollar = 104.80;
+var riyal = 28;
+console.log("Total currency in PKR: "+((dollar*10)+(riyal*25)));
+
+// Task 10
+var number = 5;
+console.log("Value of number initialized: "+number);
+console.log("After performing all the calculations: "+number+5*10/2);
+
+// Task 11
+var curentYear = 2016;
+var birthYear = 1992;
+console.log("Your age is "+(curentYear-birthYear));
+
+// task 12
+var radius = 20;
+console.log("Radius of a circle is "+radius);
+console.log("The circumference is "+(2*3.142*radius));
+console.log("The area is "+(3.142*radius*radius));
+
+// task 13
+var currentAge = 15;
+var maxAge = 65;
+var amount = 3;
+var snacks = "Chocolate Chips";
+console.log("you will need "+((maxAge-currentAge)*amount)+" "+snacks+" to last you until the ripe old age of "+maxAge);
