459c45bf21edc8a70c0f42de2f9558c0fa119364
iMubashirSyed <121051599+iMubashirSyed@users.noreply.github.com>
2024-08-19 15:55:22 +0500

    Add files via upload

---
+++ Chapter1/Chapter1.js
@@ -0,0 +1,13 @@
+// Task 1
+var name1 = "Mubashir";
+alert("Good Evening, " + name1 + "!");
+
+// Task 2
+alert("Error! Please enter a valid password");
+
+// Task 3
+alert("Welcome to JS Land..\nHappy Coding!");
+
+// Task 4
+alert("Welcome to JS Land");
+alert("Happy Coding!");
