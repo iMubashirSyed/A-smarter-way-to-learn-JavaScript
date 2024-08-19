a00e2ee0b0f16ff8cf5d8fe35a1b0180886318bb
iMubashirSyed <121051599+iMubashirSyed@users.noreply.github.com>
2024-08-19 15:57:25 +0500

    Add files via upload

---
+++ Chapter4/chapter4.js
@@ -0,0 +1,23 @@
+// Task 1
+var abc, pqr, mnq;
+
+// Task 2
+
+// legal variable names
+var myName;
+var my_Name;
+var $hello;
+var _hello;
+var abc123;
+
+// illegal variable names
+var 123abc;
+var my name;
+var new;
+var last-name;
+
+// Task 3
+console.log("Variables can only contain numbers, letter, $, and _ . For example my_1stVariable");
+console.log("Variable must begin with a letter, $ or _ . For example: name, $name, _name");
+console.log("Variables names are case sensitive");
+console.log("Variable names should not be JS keywords.");
